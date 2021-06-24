703b1b7220b0b54121e204a9efef79b4990c86a1
Bruno Amorim <bruno.amorim032@gmail.com>
2021-06-24 00:06:23 -0300

    update quarta a noite

--- projetofinal.py
+++ projetofinal.py
@@ -12,18 +12,22 @@ diasferias = int(input("Digite o número de dias de férias: "))
 # COLOCA ESSE E O DO IRFF DENTRO DE UMA FUNÇÃO para ficar mais clean
 if salariobruto <= 1045:
     salarioseminss = salariobruto - (0.075*salariobruto)
-
+    aliquotainss = 0.075
+    
 if salariobruto >= 1045.01 and salariobruto <= 2089.60:
     salarioseminss = salariobruto - (0.09*salariobruto)
+    aliquotainss = 0.09
 
 if salariobruto >= 2089.61 and salariobruto <= 3134.40:
     salarioseminss = salariobruto - (0.12*salariobruto)
+    aliquotainss = 0.12
 
 if salariobruto >= 3134.41:
     salarioseminss = salariobruto - (0.14*salariobruto)
+    aliquotainss = 0.14
 
 
-# Calculo substração do IRFF
+# Calculo substração do IRFF e férias
 
 if salarioseminss >= 1903.99 and salarioseminss <= 2826.6:
     salarioseminsssemirff = salarioseminss - (0.057*salarioseminss)
@@ -41,11 +45,29 @@ if salarioseminss >= 4664.69:
     salarioseminsssemirff = salarioseminss - (0.275*salarioseminss)
     aliquotairrf = 0.275
 
+
+#CALCULO DAS FÉRIAS
+
+if meses < 12:
+    feriasSituacao = 'Não é elegível a retirar férias com menos de 12 meses trabalhados via CLT'
+else:
+    feriasSituacao = 'Elegível a férias!'
+    um_terco = (salariobruto/3)
+    valor_dias_ferias = (salariobruto/30)*diasferias
+    provento = (valor_dias_ferias + um_terco)
+    desconto = (salariobruto - (salariobruto*aliquotainss)) + (salariobruto - (salariobruto-aliquotairrf))
+    ferias = provento - desconto
+    
+    
+
+    
+
 valordeinss = salariobruto - salarioseminss
 valordabasedecalculo = salariobruto - valordeinss
-# valoradeduzir = 
-valordeirrf= (valordabasedecalculo * aliquotairrf) * valoradeduzir
+valoradeduzir = salariobruto - salarioseminsssemirff
+valordeirrf = (valordabasedecalculo * aliquotairrf) * valoradeduzir
 
+# Impressão no txt
 with open('Valores.txt', 'a') as arquivo:
     arquivo.write('Nome: ' + nome + '\n')
     arquivo.write('Setor: ' + setor + '\n')
@@ -56,7 +78,6 @@ with open('Valores.txt', 'a') as arquivo:
 
     arquivo.write('INSS:' + '\n')
     arquivo.write('Valor total bruto: ' + str(salariobruto + bonus) + '\n')
-    arquivo.write('Salário sem INSS: ' + str(salarioseminss) + '\n')
     arquivo.write('Valor de INSS: ' + str(valordeinss) + '\n')
     arquivo.write('Valor da base de cálculo: ' + str(valordabasedecalculo) + '\n')
 
@@ -64,18 +85,19 @@ with open('Valores.txt', 'a') as arquivo:
 
     arquivo.write('IRFF:' + '\n')
     # arquivo.write('Valor a deduzir:' + str(valoradeduzir) '\n')
-    arquivo.write('Valor de IRRF: ' + valordeirrf +'\n')
-    arquivo.write('Valor Salário líquido: ' + valordeirrf +'\n')
+    arquivo.write('Valor de IRRF: ' + str(valordeirrf) +'\n')
+    arquivo.write('Valor a deduzir: ' + str(valordeirrf) +'\n')
+    arquivo.write('Valor Salário líquido: ' + str(valordeirrf) +'\n')
 
     arquivo.write('\n')
 
-    arquivo.write('FÉRIAS:' + '\n')
+    arquivo.write('FÉRIAS:' + str(feriasSituacao) + '\n')
     arquivo.write('Valor total de férias :' + '\n')
     arquivo.write('Valor de INSS de férias :' + '\n')
     arquivo.write('Valor da base da Cálculo de Férias :' + '\n')
     arquivo.write('Valor a deduzir de Férias :' + '\n')
     arquivo.write('Valor de IRRF de férias :' + '\n')
-    arquivo.write('Férias líquido :' + '\n')
+    arquivo.write('Férias líquido :' + str(ferias) + '\n')
     
     arquivo.write('\n')
     
